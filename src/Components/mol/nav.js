b721d78ad0b67d913c5df173ab9500854e98a17a
Jeshurun <jeshurunjet@gmail.com>
2019-11-26 13:01:19 +1300

    Added a Sidebar Menu

--- src/Components/mol/nav.js
+++ src/Components/mol/nav.js
@@ -5,6 +5,7 @@ import '../../css/global.css';
 
 import '../../css/lib/nav.css';
 
+import { slide as Menu } from 'react-burger-menu'
 class Navigation extends React.Component {
     render() {
         return(
@@ -12,10 +13,27 @@ class Navigation extends React.Component {
                 <Link to="/">Home</Link>
                     <Link to="/portfolio">Our Work</Link>
                     <Link to="/about">About</Link>
-                <div className="btn-menu" onClick={console.log('isClicked')}><i className="fa fa-bars"></i></div>
+                <div className="btn-menu" onClick={console.log('isClicked')}><i className="fa fa-bars"><ToggleMenu /></i></div>
             </div>
         );
     }
 }
+
+class ToggleMenu extends React.Component {
+    showSettings (event) {
+      event.preventDefault();
+    }
+  
+    render () {
+      // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+      return (
+        <Menu right>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/portfolio">Our Work</a>
+          <a id="contact" className="menu-item" href="/about">About</a>
+        </Menu>
+      );
+    }
+  }
 
 export default Navigation;
